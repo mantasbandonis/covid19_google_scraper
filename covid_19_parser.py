57da7ce4b4a76796505dd833f40bdbb686c48191
mantasbandonis <mantas.bandonis@outlook.com>
2020-03-28 20:28:08 +0100

    added number for deaths

--- covid_19_parser.py
+++ covid_19_parser.py
@@ -8,32 +8,46 @@ table_body = table.find('tbody')
 rows = table_body.findAll('tr')
 
 
-# Params cases ( yes or no to display), recovered ( yes or no to display)
-def covidaustria(cases: int, recovered: int):
+# Parameters 1 = display , 0 = dont display
+
+def covidaustria(cases: int, recovered: int, dead: int):
     for row in rows:
         if row.td.img['src'] == "https://www.gstatic.com/onebox/sports/logos/flags/austria_icon_square.svg":
             if cases == 1:
                 cases = row.findAll('td', {"class": "table_card_cell_col_1 table_card_cell_int_type"})
                 for tag in cases:
-                    casesstr = str(tag.contents[0])
-                    casesstr = casesstr.strip()
-                    casesstr = casesstr.replace(',', '')
-                    print("Total Cases in Austria: {}".format(casesstr))
+                    cases = str(tag.contents[0])
+                    cases = cases.strip()
+                    cases = cases.replace(',', '')
+                    print("Total Cases in Austria: {}".format(cases))
             elif cases == 0:
-                casesstr = "No search for Cases"
+                cases = "No search for Cases"
             else:
-                pass
+                raise ValueError("Please put either 1 (yes, display) or 0 (no display)")
+
 
             if recovered == 1:
                 recovered = row.findAll('td', {"class": "table_card_cell_col_3 table_card_cell_int_type"})
                 for tag2 in recovered:
-                    recovstr = str(tag2.contents[0])
-                    recovstr = recovstr.strip()
-                    recovstr = recovstr.replace(',', '')
-                    print("Total Recovered in Austria: {}".format(recovstr))
+                    recovered = str(tag2.contents[0])
+                    recovered = recovered.strip()
+                    print("Total Recovered in Austria: {}".format(recovered))
             elif recovered == 0:
-                recovstr = "No search for Recovered"
-            else:
-                pass
+                recovered = "No search for Recovered"
 
-            return casesstr, recovstr
+            else:
+                raise ValueError("Please put either 1 (yes, display) or 0 (no display)")
+
+            if dead == 1:
+                dead = row.findAll('td', {"class": "table_card_cell_col_4 table_card_cell_int_type"})
+                for tag3 in dead:
+                    dead = str(tag3.contents[0])
+                    dead = dead.strip()
+                    print("Total Deaths in Austria: {}".format(dead))
+            elif dead == 0:
+                dead = "No search for Dead"
+
+            else:
+                raise ValueError("Please put either 1 (yes, display) or 0 (no display)")
+
+            return cases, recovered, dead
